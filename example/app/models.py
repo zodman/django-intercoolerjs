07badd448949d412a336746431bee1c3f03356a2
Your Name <you@example.com>
2019-05-01 15:56:38 -0500

    udapte forms

--- example/app/models.py
+++ example/app/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 class Entry(models.Model):
-    name = models.CharField(max_length=30, help_text="Entry help text")
+    name = models.CharField(unique=True, max_length=30, help_text="Entry help text")
+
+    def __str__(self):
+        return self.name
 
 class Address(models.Model):
-    street = models.CharField(max_length=30)
+    street = models.CharField(unique=True, max_length=30)
     entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
     line_one = models.TextField()
     number  = models.PositiveIntegerField()
--- example/app/views.py
+++ example/app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django import forms
 from .models import Address, Entry
-
+from django.contrib import messages
 
 
 class EntryForm(forms.ModelForm):
@@ -13,14 +13,23 @@ class AddressForm(forms.ModelForm):
     class Meta:
         model = Address
         exclude = ()
+    
 
 def home(request):
     context = {
         'form': AddressForm(),
         'form1': EntryForm()
     }
-    if request.POST and context["form1"].is_valid() \
-                and context["form"].is_valid():
-        pass
+    context['form'].fields["entry"].query = Entry.objects.all()
+    if request.method == "POST":
+        context["form1"] = EntryForm(request.POST)
+        context["form"] = AddressForm(request.POST)
+        if context["form1"].is_valid():
+            entry = context["form1"].save()
+            messages.info(request,"Entry {} salvado".format(entry.id))
+        if context["form"].is_valid():
+            entry = context["form"].save()
+            messages.info(request,"Address {} salvado".format(entry.id))
 
+    context["addresses"] = Address.objects.all()
     return render(request, "base.html", context)
